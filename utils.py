65f7dc54d6cb923c83a7fe61a3f5c33fcc418367
Ruihan Wu <ruihan.wu14@gmail.com>
2025-02-12 07:18:30 +0000

    config

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import copy
 import numpy as np
 from scipy.stats import sem, hmean, ks_2samp
 from natsort import natsorted
-def get_model_identifiers_from_yaml(model_family):
+def get_model_identifiers_from_yaml(model_family, config_path="config"):
     #path is model_configs.yaml
     '''
     models:
@@ -15,7 +15,7 @@ def get_model_identifiers_from_yaml(model_family):
             start_of_sequence_token: "<s>"
     '''
     model_configs  = {}
-    with open("config/model_config.yaml", "r") as f:
+    with open(f"{config_path}/model_config.yaml", "r") as f:
         model_configs = yaml.load(f, Loader=yaml.FullLoader)
     return model_configs[model_family]
 
